d360b82acb54b8f82ab05d7999302a5ec1094168
cmdr <zacharyl2005@gmail.com>
2026-04-01 17:24:42 -0400

    Patched the ability to solve an extra free character

--- cipher.py
+++ cipher.py
@@ -7,10 +7,10 @@ length = len(flag)
 for i in range(int(input("copys:"))):
 
     # Generating key
-    key = random.sample(range(1, (length+1) * 3, 3), length)
+    key = random.sample(range(3, (length+1) * 3, 3), length)
 
     # Encoding Flag
     temFlag = list(flag)
     for x in range(length):
-        temFlag[x] = ord(temFlag[x]) * key[x]
+        temFlag[x] = (ord(temFlag[x]) * key[x]) % 256
     print(temFlag)
